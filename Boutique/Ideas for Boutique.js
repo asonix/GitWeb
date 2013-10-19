8f54d318deb06599e607476d98444fb2bb689cd0
Asonix <asonix.music@gmail.com>
2013-10-19 17:21:05 -0500

    Downloaded jquery-2.0.3.js
    Also changed some code. Mobile site is working gloriously from what I
    can tell. Haven't actually tested it on a mobile platform...

--- Boutique/Ideas for Boutique.js
+++ Boutique/Ideas for Boutique.js
@@ -1,57 +1,106 @@
 $(document).ready(function() {
-        
-    //On Page Load
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('head').append('<link href="Ideas for Boutique mobile.css" type="text/css" rel="stylesheet"/>');
-        $('.wrapper').css('height', String(Math.ceil($(window).height()-25)));
-    }
-    
-    else {
-        $('head').append('<link href="Ideas for Boutique.css" type="text/css" rel="stylesheet"/>')
-        
-        if ($('body').width() < 637) {
-            $('.wrapper').css('width', '100%').css('height', String(Math.ceil($(window).height()-40)));
-            $('.footer').css('position', 'absolute').css('bottom', '0px');
-            
-            if ($('html').height() < 611) {
-                $('.footer').css('position', '').css('bottom', '');
-                $('.wrapper').css('height', '');
-            }
+
+        //On Page Load
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                $('head').append('<link href="Ideas for Boutique mobile.css" type="text/css" rel="stylesheet"/>');
+                var i = 0;
+                var ratio = 2.625;
+                var height = String(Math.round($('.content').width()/ratio))+'px';
+                var logofs = String(Math.round($(window).width()/3.11))+'px';
+                var defaultfs = String(Math.round($(window).width()/31.1))+'px';
+                var hdefaultfs = String(Math.round($(window).width()/62.2))+'px';
+                var qdefaultfs = String(Math.round($(window).width()/124.4))+'px';
+                console.log(height);
+                var m = setInterval(function() {
+                   if (i < 2) {
+                        height = String(Math.round($('.content').width()/ratio))+'px';
+                        logofs = String(Math.round($(window).width()/3.11))+'px';
+                        defaultfs = String(Math.round($(window).width()/31.1))+'px';
+                        hdefaultfs = String(Math.round($(window).width()/62.2))+'px';
+                        qdefaultfs = String(Math.round($(window).width()/124.4))+'px';
+                        $('.content').css('height', height);
+                        $('body').css('height', String(Math.round($('.wrapper').height()+$('.formalities').height()))+'px').css('min-height', String(Math.round($('body').height()))+'px').css('min-width', String(Math.round($('body').width()))+'px');
+                        $('.logo').css('font-size', logofs);
+                        $('html').css('font-size', defaultfs);
+                        $('.wrapper').css('box-shadow', '0px 0px '+hdefaultfs+' #000000');
+                        $('nav').css('margin-bottom', defaultfs).css('margin-top', hdefaultfs);
+                        $('.navlink').css('margin-left', qdefaultfs).css('margin-right', qdefaultfs);
+                        i++;
+                   }
+                   else {
+                        clearInterval(m);
+                   }
+                });
         }
         
         else {
-            $('.wrapper').css('width', '70%').css('height', '');
-            $('.footer').css('position', '').css('bottom', '');
-        }
-        
-        var ratio = 2.625;
-        var height = String(Math.ceil($('.content').width()/ratio))+'px';
-        $('.content').css('height', height);
-        $('.wrapper').css('width', $('wrapper').width());
-        
-        //If window is resized
-        $(window).resize(function() {
+                $('head').append('<link href="Ideas for Boutique.css" type="text/css" rel="stylesheet"/>')
                 
-            if ($('body').width() < 637) {
-                $('.wrapper').css('width', '100%').css('height', String(Math.ceil($(window).height()-40)));
-                $('.footer').css('position', 'absolute').css('bottom', '0px');
-                
-                if ($('html').height() < 611) {
-                    $('.footer').css('position', '').css('bottom', '');
-                    $('.wrapper').css('height', '');
+                if ($('body').width() < 637) {
+                        $('.wrapper').css('width', '100%').css('height', String(Math.round($(window).height()-40))).css('margin-top', '0px');
+                        $('.footer').css('position', 'absolute').css('bottom', '0px');
+                        
+                        if ($('html').height() < 611) {
+                                $('.footer').css('position', '').css('bottom', '');
+                                $('.wrapper').css('height', '');
+                                $('.formalities').css('position', 'absolute');
+                        }
                 }
-            }
-            
-            else {
-                $('.wrapper').css('width', '70%');
-                $('.footer').css('position', '');
-                $('.footer').css('bottom', '');
-                $('.wrapper').css('height', '');
-            }
-            
-            height = String(Math.ceil($('.content').width()/ratio))+'px';
-            $('.content').css('height', height);
-            console.log(height);
-        });
-    }
+                
+                else {
+                        $('.wrapper').css('width', '70%').css('height', '').css('margin-top','4%');
+                        $('.footer').css('position', '').css('bottom', '');
+                }
+                
+                var i = 0;
+                var navwidth = String(Math.round($('#linkone').width()+$('#linktwo').width()+$('#linkthree').width()+60))+'px';
+                var ratio = 2.625;
+                var height = String(Math.round($('.content').width()/ratio))+'px';
+                var m = setInterval(function() {
+                        if (i < 2) {
+                                height = String(Math.round($('.content').width()/ratio))+'px';
+                                $('.content').css('height', height);
+                                $('.wrapper').css('width', $('wrapper').width());
+                                navwidth = String(Math.round($('#linkone').width()+$('#linktwo').width()+$('#linkthree').width()+80))+'px';
+                                $('nav').css('width', navwidth);
+                                i++;
+                        }
+                        else {
+                                clearInterval(m);
+                        }
+                }, 25)
+                
+                
+                //If window is resized
+                $(window).resize(function() {
+                
+                        if ($('body').width() < 637) {
+                                $('.wrapper').css('width', '100%').css('height', String(Math.round($(window).height()-40))).css('margin-top', '0px');
+                                $('.footer').css('position', 'absolute').css('bottom', '0px');
+                                
+                                if ($('html').height() < 611) {
+                                        $('.footer').css('position', '').css('bottom', '');
+                                        $('.wrapper').css('height', '');
+                                        $('.formalities').css('position', 'absolute');
+                                }
+                                else {
+                                        $('.formalities').css('position', 'fixed');
+                                }
+                                
+                                if ($(window).width() < 471) {
+                                        $('.formalities').css('position', 'absolute');
+                                }
+                        }
+                        
+                        else {
+                                $('.wrapper').css('width', '70%').css('height', '').css('margin-top', '4%');
+                                $('.footer').css('position', '').css('bottom', '');
+                                $('.formalities').css('position', 'fixed');
+                        }
+                        
+                        height = String(Math.round($('.content').width()/ratio))+'px';
+                        $('.content').css('height', height);
+                        console.log(height);
+                });
+        }
 });
